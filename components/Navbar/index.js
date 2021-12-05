3d9088877d124c1fc62398c19112dfa59864cea1
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2021-12-04 20:36:43 -0500

    add photo perfil

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -8,8 +8,7 @@ export default function Navbar({ state = 1 }) {
             <nav className={styles.nav}>
                 <ul className={styles.list_ul}>
                     <li className={`${styles.list_li_resize} ${state === 1 ? `${styles.active_ref}` : ''}`}><Link href="/"><a className={styles.nav_a}>Sobre mi</a></Link></li>
-                    <li className={`${styles.list_li_resize} ${state === 2 ? `${styles.active_ref}` : ''}`}><Link href="/repositorios"><a className={styles.nav_a}>Repositorios</a></Link></li>
-                    <li className={`${state === 3 ? `${styles.active_ref}` : ''}`}><Link href="/proyectos"><a className={styles.nav_a}>Proyectos</a></Link></li>
+                    <li className={`${state === 2 ? `${styles.active_ref}` : ''}`}><Link href="/contactos"><a className={styles.nav_a}>Contactos</a></Link></li>
                 </ul>
             </nav>
         </header >
